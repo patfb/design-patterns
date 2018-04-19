abcfe4ca536030761ce47c41f7704c6acffaf99f
Patrick <patrick.f.boyne@gmail.com>
2018-04-18 21:28:35 -0400

    add bridge demo example

---
+++ src/main/java/bridge/shape/with/bridge/Color.java
@@ -0,0 +1,7 @@
+package bridge.shape.with.bridge;
+
+public interface Color {
+
+	public void applyColor();
+	
+}
---
+++ src/main/java/bridge/shape/with/bridge/Red.java
@@ -0,0 +1,10 @@
+package bridge.shape.with.bridge;
+
+public class Red implements Color {
+
+	@Override
+	public void applyColor() {
+		System.out.println("Applying red color");
+	}
+
+}
